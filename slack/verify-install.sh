db0a41bd6fc53e9f97a5c8a12c0b200729509c09
Teresa Nesteby <teresa.nesteby@gmail.com>
2016-06-13 16:53:07 -0700

    comments in description of code

--- slack/verify-install.sh
+++ slack/verify-install.sh
@@ -3,5 +3,4 @@
 
 #first test checks to see if slack already installed through construct
 #second test checks to see if slack was installed directly
-test -d $HOME/Applications/"Slack.app" ||
-test -d /Applications/"Slack.app"
+test -d $HOME/Applications/"Slack.app" || test -d /Applications/"Slack.app"
--- virtualbox/verify-install.sh
+++ virtualbox/verify-install.sh
@@ -1,3 +1,1 @@
 test -d $HOME/Applications/"Virtualbox.app"
-
-# brew cask list | grep --quiet -x virtualbox
